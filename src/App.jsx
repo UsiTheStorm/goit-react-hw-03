1ea3c71b9d82ce5f7a527161a4bafdd133a0c2fc
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-07-20 09:13:02 +0100

    Add basic phonebook component structure
    Introduced ContactForm, ContactList, and SearchBox components and integrated them into App.jsx to set up the initial structure for the phonebook application.

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,19 @@ import { useState } from 'react';
 
 import './App.css';
 
+import ContactForm from './components/ContactForm';
+import ContactList from './components/ContactList';
+import SearchBox from './components/SearchBox';
+
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
 }
 
 export default App;
