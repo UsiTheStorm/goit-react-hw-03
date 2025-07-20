95bc0a5c73cdbde947082d06b05601ab73016680
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-07-20 10:05:17 +0100

    Add component static layout

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,13 @@ import ContactList from './components/ContactList';
 import SearchBox from './components/SearchBox';
 import contactList from './data/contacts.json';
 
-console.log(contactList);
-
 function App() {
   return (
     <div>
       <h1>Phonebook</h1>
       <ContactForm />
       <SearchBox />
-      <ContactList />
+      <ContactList contacts={contactList} />
     </div>
   );
 }
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import { useState } from 'react';
 
-function ContactList() {
-  return <div>ContactList</div>;
+function Contact({ contact }) {
+  const { name, number } = contact;
+  console.log(name);
+  return (
+    <li>
+      <div>
+        <div className="field">{name}</div>
+        <div className="field">{number}</div>
+      </div>
+      <button className="btn">Delete</button>
+    </li>
+  );
+}
+
+function ContactList({ contacts }) {
+  return (
+    <ul>
+      {contacts.map((contact) => (
+        <Contact contact={contact} key={contact.id} />
+      ))}
+    </ul>
+  );
 }
 
 export default ContactList;
