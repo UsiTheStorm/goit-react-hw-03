5b89bfa4bb1c571a9658e247de36a628e15abcc7
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-07-20 09:47:42 +0100

    Add static component layout

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function SearchBox() {
-  return <div>SearchBox</div>;
+  return (
+    <div>
+      <label htmlFor="searchBox">Find contacts by name</label>
+      <input type="text" name="searchBox" id="searchBox" />
+    </div>
+  );
 }
 
 export default SearchBox;
