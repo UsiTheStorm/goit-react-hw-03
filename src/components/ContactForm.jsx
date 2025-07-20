0ba09d428aa68fa90e67e748143d0dd755d47566
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-07-20 09:41:45 +0100

    Add static layout of component

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 function ContactForm() {
-  return <div>ContactForm</div>;
+  return (
+    <div className="contact-form">
+      <div className="contact-form__field">
+        <label htmlFor="contactName">Name</label>
+        <input type="text" name="contactName" id="contactName" />
+      </div>
+
+      <div className="contact-form__field">
+        <label htmlFor="contactNumber">Number</label>
+        <input type="text" name="contactNumber" id="contactNumber" />
+      </div>
+
+      <button type="submit">Add contact</button>
+    </div>
+  );
 }
 
 export default ContactForm;
